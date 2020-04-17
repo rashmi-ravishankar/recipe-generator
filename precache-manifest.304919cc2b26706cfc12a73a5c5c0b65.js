da7f85748144a28416886fe738057d864f8524e6
R. Ravishankar <ravishri@mail.uc.edu>
2020-04-17 19:53:09 -0400

    Updates

---
+++ precache-manifest.304919cc2b26706cfc12a73a5c5c0b65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58da1494e8c1ddb50da1ca3652053592",
+    "url": "/recipe-generator/index.html"
+  },
+  {
+    "revision": "2e83729e2e5729a2adcd",
+    "url": "/recipe-generator/static/css/main.830dd698.chunk.css"
+  },
+  {
+    "revision": "89c17f2427c691f1a957",
+    "url": "/recipe-generator/static/js/2.6d7f4ea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-generator/static/js/2.6d7f4ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e83729e2e5729a2adcd",
+    "url": "/recipe-generator/static/js/main.c1c41c63.chunk.js"
+  },
+  {
+    "revision": "ccfd2c80f4b63770c1ec",
+    "url": "/recipe-generator/static/js/runtime-main.96de2913.js"
+  },
+  {
+    "revision": "f52bcbd854652df11164c824967a6307",
+    "url": "/recipe-generator/static/media/chevronTeal.f52bcbd8.gif"
+  }
+]);
